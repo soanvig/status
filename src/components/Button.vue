cb99c34fbc1752f2e68ecc256f2cfb7838a33031
Soanvig <soanvig@gmail.com>
2020-12-06 17:59:36 +0100

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="mdc-button mdc-button--raised">
+    <div class="mdc-button__ripple"></div>
+    <span class="mdc-button__label">
+      <slot />
+    </span>
+  </button>
+</template>
+<script>
+import '@material/button/dist/mdc.button.min.css';
+import { MDCRipple } from '@material/ripple';
+
+export default {
+  name: 'button',
+  mounted () {
+    MDCRipple.attachTo(this.$el);
+  }
+}
+</script>
